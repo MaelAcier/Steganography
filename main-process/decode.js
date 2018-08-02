87f263db0c02bfe2c9bc59706764060ea3207da2
Maël Acier <mael.acier@protonmail.com>
2018-08-02 20:36:33 +0200

    added header support

--- main-process/decode.js
+++ main-process/decode.js
@@ -1,48 +1,62 @@
 var fs = require('fs')
 var path = require('path')
 
-const img = path.join( __dirname , "../img_test.bmp" )
-const txt = path.join( __dirname , "../txt_test.txt" )
-const writeStream = fs.createWriteStream(txt)
+const img = path.join( __dirname , "../test/img_test.bmp" )
 
 decode(img)
 
-var message = ""
-
-
 function decode( file ) {
     console.log(`File to decode: ${file}`)
-
+    
     fs.readFile( file, function(err, buffer) {  
-        if (err) throw err 
-        extract( buffer, (binary) =>{
-            analyze(binary, (message) => {
-                console.log(message)
-                writeStream.write(message)
-            })
+        if (err) throw err
+        var messageLength
+        var titleLength
+        var title
+        var message
+        extract( buffer, 0, 64, (bin) =>{
+            messageLength = parseInt(bin, 2)
         })
+        extract( buffer, 64, 72, (bin) =>{
+            titleLength = parseInt(bin, 2)
+        })
+        if (titleLength > 0){
+            extract( buffer, 72, 72+titleLength, (bin) =>{
+                title = bin
+            })
+        } else title = undefined
+        extract( buffer, 72+titleLength, 72+titleLength+messageLength*8, (bin) =>{
+            message = bin
+            message = analyze(bin)
+        })
+        console.log(title)
+        console.log(message)
     })
 }
 
-
-function extract( buffer, callback ){
-    buffer = buffer.slice(54)
-    let size = buffer.length/10
-    console.log(size)
-    let data = ""
+function extract( buffer, start, end, callback ){
+    start /= 2
+    end /= 2
+    buffer = buffer.slice(54 + start, 54 + end)
+    let size = end - start
+    var data = ""
     for (i=0;i<size;i++){
-        let extract = (buffer[i]%4).toString(2)
-        data += ( "00" + extract ).substring( extract.length )
+        let bin = (buffer[i]%4).toString(2)
+        data += addZero( bin, 2 )
     }
     callback(data)
 }
 
-function analyze( binary, callback){
+function analyze( binary ){
     let bytes = binary.match(/.{1,8}/g)
+    var output = ""
     for (i=0;i<bytes.length;i++){
         let byte = parseInt(bytes[i], 2)
-        message += String.fromCharCode( byte )
+        output += String.fromCharCode( byte )
     }
-    callback(message)
+    return output
 }
 
+function addZero( string, size ) {
+    return string.length >= size ? string : new Array(size - string.length + 1).join("0") + string
+}
--- main-process/encode.js
+++ main-process/encode.js
@@ -1,9 +1,9 @@
 var fs = require('fs')
 var path = require('path')
 
-var secret = `THIS IS A SECRET`
+const secret = `THIS IS A SECRET`
 
-const img = path.join( __dirname , "../img_test.bmp" )
+const img = path.join( __dirname , "../test/img_test.bmp" )
 
 //encode( secret, img )
 
@@ -11,26 +11,54 @@ function encode( message, file ) {
     console.log(`Message to encode: ${message}`)
     console.log(`File to encode: ${file}`)
 
-    const writeStream = fs.createWriteStream(file, {flags: 'r+', start:54})
-    let binaryToEncode = []
+    let title = ""
+    let bin = header( message, title ) + messageToBinary( title ) + messageToBinary( message )
+    bin = cutTwoDigits( bin )
 
-    for(i=0;i<message.length;i++) {
-        let letterToASCII = message.charCodeAt(i)
-        let ASCIItoBinary = parseInt(letterToASCII, 10).toString(2)
-        let binaryToByte = ( "00000000" + ASCIItoBinary ).substring( ASCIItoBinary.length )
-        let decompose = binaryToByte.match(/.{1,2}/g)
-        binaryToEncode = binaryToEncode.concat(decompose)
-    }
+    const writeStream = fs.createWriteStream(file, {flags: 'r+', start:54})
 
     fs.readFile(file, function(err, buffer) {  
         if (err) throw err
-        buffer = buffer.slice(54, 54+ binaryToEncode.length)
-        
-        for (i=0;i<buffer.length;i++){
-            buffer[i] -= buffer[i]%4
-            buffer[i] += parseInt(binaryToEncode[i], 2)
-        }
+        buffer = transform( bin, buffer )
 
         writeStream.write(buffer)
     })
+}
+
+function messageToBinary( message ) {
+    var output = ""
+    for( i=0; i<message.length; i++ ) {
+        let letterToASCII = message.charCodeAt(i)
+        let ASCIItoBinary = parseInt(letterToASCII, 10).toString(2)
+        output += addZero( ASCIItoBinary, 8 )
+    }
+    return output
+}
+
+function transform( binary, buffer) {
+    buffer = buffer.slice(54, 54+ binary.length)
+        
+    for (i=0;i<buffer.length;i++){
+        buffer[i] -= buffer[i]%4
+        buffer[i] += parseInt(binary[i], 2)
+    }
+    return buffer
+}
+
+function header( message, title ) {
+    let messageLength = addZero( message.length.toString(2), 64 )
+    if (title !== undefined){
+        var titleLength = addZero( title.length.toString(2), 8 )
+    } else {
+        var titleLength = addZero( "".toString(2), 8 )
+    }
+    return messageLength + titleLength
+}
+
+function addZero( string, size ) {
+    return string.length >= size ? string : new Array(size - string.length + 1).join("0") + string
+}
+
+function cutTwoDigits ( data ) {
+    return data.match(/.{1,2}/g)
 }
